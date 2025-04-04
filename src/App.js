051bdb38df243be6c5dccc5e10cc0fe6c82dfcc4
genieprinters <runner0090@yahoo.com>
2025-04-04 06:50:37 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Genie Printers Cost Calculator</h1>
+      <p>Welcome! The app is now working.</p>
+    </div>
+  );
+}
+
+export default App;
