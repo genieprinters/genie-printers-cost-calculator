d751763378811dc1d9f9953e1fdca45b8b7b5d55
genieprinters <runner0090@yahoo.com>
2025-04-04 05:52:06 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";  // Add CSS file for styling if you have one
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
